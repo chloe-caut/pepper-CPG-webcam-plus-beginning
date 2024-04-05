b7c9fdfa1b178d0b42b618d418823d8123a3e9f3
chloe <chloe.caut@gmail.com>
2024-04-05 14:26:39 +0200

    juste la synchro

--- pose-detect.py
+++ pose-detect.py
@@ -1,7 +1,11 @@
+import numpy as np
+import cv2
+import time
 
-# premier test code propsé par mediapipe.
-# a mettre dans une autre branche que main par contre
+# premier test code propsé par mediapipe / c'est quoi cette version??? 
 
+
+'''
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import numpy as np
@@ -26,3 +30,40 @@ def draw_landmarks_on_image(rgb_image, detection_result):
       solutions.pose.POSE_CONNECTIONS,
       solutions.drawing_styles.get_default_pose_landmarks_style())
   return annotated_image
+  '''
+
+# instructions suivies depuis ici: 
+#https://developers.google.com/mediapipe/solutions/vision/pose_landmarker/python
+
+
+import mediapipe as mp
+
+#pas sure que ces 2 la soient necessaires, elles ne sont pas dans le hand detect
+from mediapipe.tasks import python 
+from mediapipe.tasks.python import vision
+
+#model : pose_landmark_lite.task déjà dans le dossier
+model_path= pose_landmark_lite.task #on peut ecrire ca comme ca?
+#create the task section: on est en live stream
+import mediapipe as mp
+
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a pose landmarker instance with the live stream mode:
+def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('pose landmarker result: {}'.format(result))
+
+options = PoseLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+
+with PoseLandmarker.create_from_options(options) as landmarker:
+  # The landmarker is initialized. Use it here.
+  # ...
+
+
